23b62a4166654dfed62273affa8713dac6dd2462
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2018-02-08 15:53:49 -0800

    Add mechanism to check for keys in keyDefinition list

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 "use strict";
 
-var errorMessage = "Invalid use of API key";
+var keyDefinitions = require("./key-definitions");
+
+var map = {};
+keyDefinitions.forEach(function(def)
+{
+    map[def.description] = new RegExp(def.regexp.pattern);
+    return map[def.description];
+});
+
+function containsKey(string)
+{
+    for (var keyDef in map)
+    {
+        if (map.hasOwnProperty(keyDef))
+        {
+            if (map[keyDef].test(string))
+                return keyDef;
+        }
+    }
+    return false;
+}
+
+function errorMessage(keyDescription)
+{
+    return "Error: Contains " + keyDescription + ".";
+}
 
 module.exports = function ()
 {
@@ -11,14 +36,16 @@ module.exports = function ()
                 // As far as I can tell, the only way for a Visitor to get to a File is through the Program parent
                 path.parent.comments.forEach(function(comment)
                 {
-                    if (comment.value === " API_KEY")
-                        throw path.buildCodeFrameError(errorMessage);
+                    var key = containsKey(comment.value);
+                    if (key)
+                        throw path.buildCodeFrameError(errorMessage(key));
                 });
             },
             StringLiteral(path)
             {
-                if (path.node.value === "API_KEY")
-                    throw path.buildCodeFrameError(errorMessage);
+                var key = containsKey(path.node.value);
+                if (key)
+                    throw path.buildCodeFrameError(errorMessage(key));
             },
         }
     };
