0c25d74c13d11b896727b4e536fbe49f586851df
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2018-02-09 16:38:10 -0800

    Export RegEx combining all keyRegExes

--- processed-key-definitions.js
+++ processed-key-definitions.js
@@ -1,11 +1,17 @@
 var keyDefinitions = require("./key-definitions");
 
 var keyDefinitionsMap = {};
+var keyRegexes = [];
 
 keyDefinitions.forEach(function(def)
 {
     keyDefinitionsMap[def.description] = new RegExp(def.regexp.pattern);
+    keyRegexes.push(keyDefinitionsMap[def.description]);
     return keyDefinitionsMap[def.description];
 });
 
 module.exports.keyDefinitionsMap = keyDefinitionsMap;
+
+var combinedKeyRegExes = new RegExp(keyRegexes.map(function (re) { return re.source; }).join("|"));
+
+module.exports.combinedKeyRegExes = combinedKeyRegExes;
