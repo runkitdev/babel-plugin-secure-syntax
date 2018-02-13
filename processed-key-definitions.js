d311450eeeebcc234808729f11a151f1fea660d3
Pieter Ouwerkerk <pieter.ouwerkerk@gmail.com>
2018-02-12 16:30:53 -0800

    Simplify keyDefinitionsMap building

--- processed-key-definitions.js
+++ processed-key-definitions.js
@@ -1,17 +1,17 @@
 var keyDefinitions = require("./key-definitions");
 
 var keyDefinitionsMap = {};
-var keyRegexes = [];
 
-keyDefinitions.forEach(function(def)
+var keyDefinitionRegExps = keyDefinitions.map(function(def)
 {
-    keyDefinitionsMap[def.description] = new RegExp(def.regexp.pattern);
-    keyRegexes.push(keyDefinitionsMap[def.description]);
-    return keyDefinitionsMap[def.description];
+    return (keyDefinitionsMap[def.description] = new RegExp(def.regexp.pattern));
 });
 
 module.exports.keyDefinitionsMap = keyDefinitionsMap;
 
-var combinedKeyRegExes = new RegExp(keyRegexes.map(function (re) { return re.source; }).join("|"));
+var combinedKeyRegExes = new RegExp(keyDefinitionRegExps.map(function (re)
+{
+    return re.source;
+}).join("|"));
 
 module.exports.combinedKeyRegExes = combinedKeyRegExes;
